7b601b730b3d148a0e3852750268d5fcd7e12462
yash96245 <yashgediya007@gmail.com>
2022-08-27 16:55:56 +0530

    Final touch up  project

--- Js/custom.js
+++ Js/custom.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
     setTimeout(function () {
         loader.removeClass('notLoaded');
     }, 1000);;
-
     setInterval(function () {
         $("#loader").fadeOut();
     }, 1000)
@@ -65,7 +64,6 @@ $(document).ready(function () {
     //--------------- Index-banner---------------
     $('#main_banner').owlCarousel({
         loop: true,
-        margin: 10,
         nav: true,
         navText: ['<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
         //center:true,
@@ -139,12 +137,12 @@ $(document).ready(function () {
 
     //----------- Bottom to top scroll -------------
     $('.main-sroll-top').bind("click", function () {
-        $('html, body').animate({ scrollTop: 0 }, 1200);
+        $('html, body').animate({ scrollTop: 0 }, 0);
         return false;
     });
     $('.main-sroll-top').hide();
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
+        if ($(this).scrollTop() > 500) {
             $('.main-sroll-top').fadeIn();
         } else {
             $('.main-sroll-top').fadeOut();
@@ -193,28 +191,105 @@ $(document).ready(function () {
 
     });
 
-});
+    // datePicker in tour Details page
+    $(function () {
+        var from = $("#datepicker").datepicker({
+            dateFormat: "dd-mm-yy",
+            changeMonth: true,
+            minDate: 0,
+        }).on("change", function () {
+            var to = $("#todatepicker").datepicker({
+                dateFormat: "dd-mm-yy",
+                changeMonth: true,
+                minDate: 0,
+            })
+            to.datepicker("option", "minDate", getDate(this));
 
+            var toDate = new Date(getDate(this));
+            toDate.setDate(toDate.getDate() + 9);
+            to.datepicker("option", "maxDate", toDate);
+
+        })
+
+        function getDate(element) {
+            var date;
+            var dateFormat = "dd-mm-yy";
+            try {
+                date = $.datepicker.parseDate(dateFormat, element.value);
+            } catch (error) {
+                date = null;
+            }
+            return date;
+        }
+        $("#datepicker").click(function () {
+            $("#ui-datepicker-div").addClass("tour-from-date-picker");
+        })
+    });
+
+    // select-img in tour-details page
+    $("#inputGroupSelect02").change(function () {
+        _data_image = $('option:selected', this).data('image');
+        $('.active').removeClass('active');
+        $(_data_image).addClass('active');
+    });
+
+    // when select option then auto change label price in tour details page
+    $(function () {
+        $('#inputGroupSelect02').change(function () {
+            if ($('#inputGroupSelect02').val() == '2') {
+                $('#selectPriceLabel').text('$1500/-');
+            } else if
+                ($('#inputGroupSelect02').val() == '3') {
+                $('#selectPriceLabel').text('$2500/-');
+            } else if
+                ($('#inputGroupSelect02').val() == '4') {
+                $('#selectPriceLabel').text('$3000/-');
+            } else if
+                ($('#inputGroupSelect02').val() == '5') {
+                $('#selectPriceLabel').text('$2500/-');
+            } else if
+                ($('#inputGroupSelect02').val() == '6') {
+                $('#selectPriceLabel').text('$1500/-');
+            } else if
+                ($('#inputGroupSelect02').val() == '7') {
+                $('#selectPriceLabel').text('$1300/-');
+            } else {
+                $('#selectPriceLabel').text('Price');
+            }
+        });
+    });
+
+});
 
 // show password eye button
-const togglePassword = document
-    .querySelector('#togglePassword');
+$(function () {
+    $('#togglePassword').click(function () {
 
-const password = document.querySelector('#password');
+        if ($(this).hasClass('fa-eye-slash')) {
 
-togglePassword.addEventListener('click', () => {
-    const type = password
-        .getAttribute('type') === 'password' ?
-        'text' : 'password';
+            $(this).removeClass('fa-eye-slash');
 
-    password.setAttribute('type', type);
+            $(this).addClass('fa-eye');
+
+            $('#password').attr('type', 'text');
+
+        } else {
+
+            $(this).removeClass('fa-eye');
+
+            $(this).addClass('fa-eye-slash');
+
+            $('#password').attr('type', 'password');
+        }
+    });
 });
+
 
 // login admin
 function login() {
     if ($("#username").val() == "admin" && $("#password").val() == "password") {
-        window.location="./Pages/dashboard.html";
+        window.location = "./Pages/dashboard.html";
     } else {
-        window.location="./Pages/index.html";
+        window.location = "./Pages/index.html";
     }
 };
